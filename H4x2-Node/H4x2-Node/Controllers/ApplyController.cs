7d841665bbb707dd2e4afedef582ab2f0b972aff
aranthana <aru2191@gmail.com>
2022-12-13 17:07:16 +1000

    Ip from X-Forward

--- H4x2-Node/H4x2-Node/Controllers/ApplyController.cs
+++ H4x2-Node/H4x2-Node/Controllers/ApplyController.cs
@@ -50,15 +50,11 @@ namespace H4x2_Node.Controllers
         }
         private ActionResult<int> Throttle()
         {
-            var ip = Request.HttpContext.Connection.RemoteIpAddress; // Get client's IP
-            Console.WriteLine("IP address  {0}", ip.ToString());
-            Console.WriteLine("IP address IpV4 {0}", Request.HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString());
-            Console.WriteLine("IP address IPv6 {0}", Request.HttpContext.Connection.RemoteIpAddress.MapToIPv6().ToString());
-            Console.WriteLine("IP {0}", (HttpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault() ?? "").Split(new char[] { ':' }).FirstOrDefault());
-            if (ip is not null)
-                return _throttlingManager.Throttle(ip.ToString()).GetAwaiter().GetResult(); 
+            var Ip = (HttpContext.Request.Headers["X-Forwarded-For"].FirstOrDefault() ?? "").Split(new char[] { ':' }).FirstOrDefault();
+            if (!String.IsNullOrEmpty(Ip)) 
+                return _throttlingManager.Throttle(Ip.ToString()).GetAwaiter().GetResult();
             else
-               throw new Exception("IP address is null !");// return BadRequest("IP address is null !"); 
+                return _throttlingManager.Throttle(Request.HttpContext.Connection.RemoteIpAddress.ToString()).GetAwaiter().GetResult();
         }
     }
 }
