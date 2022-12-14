40f21430355b61d32461364cb1c5a6ecdac162e0
Julio <julio@tide.org>
2022-12-14 14:35:20 +1000

    add license header

--- h4x2-front/modules/H4x2-TideJS/index.js
+++ h4x2-front/modules/H4x2-TideJS/index.js
@@ -1,3 +1,21 @@
+// 
+// Tide Protocol - Infrastructure for a TRUE Zero-Trust paradigm
+// Copyright (C) 2022 Tide Foundation Ltd
+// 
+// This program is free software and is subject to the terms of 
+// the Tide Community Open Code License as published by the 
+// Tide Foundation Limited. You may modify it and redistribute 
+// it in accordance with and subject to the terms of that License.
+// This program is distributed WITHOUT WARRANTY of any kind, 
+// including without any implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE.
+// See the Tide Community Open Code License for more details.
+// You should have received a copy of the Tide Community Open 
+// Code License along with this program.
+// If not, see https://tide.org/licenses_tcoc2-0-0-en
+//
+
+
 export { default as Point } from './Ed25519/point.js';
 export { default as PrismFlow } from './Flow/Prism.js';
 export { default as NodeClient } from './Clients/NodeClient.js'
