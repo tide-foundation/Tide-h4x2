d9fdd2a12925c65debeff300215ca0b9f407b1e6
aranthana <aru2191@gmail.com>
2023-01-16 15:01:58 +1000

    Sample vendor

---
+++ H4x2-Vendor/H4x2-Vendor/Helpers/DataContext.cs
@@ -0,0 +1,21 @@
+namespace H4x2_Vendor.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using H4x2_Vendor.Entities;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    public DbSet<User> UserSecrets { get; set; }
+}
---
+++ H4x2-Vendor/H4x2-Vendor/Program.cs
@@ -0,0 +1,28 @@
+﻿using H4x2_Vendor.Helpers;
+using H4x2_Vendor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var services = builder.Services;
+services.AddDbContext<DataContext>();
+services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+if(app.Environment.IsDevelopment()) // To create table if not exists
+{
+    using(var scope = app.Services.CreateScope())
+    {
+        var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+        dataContext.Database.EnsureCreated();
+    }
+}
+
+app.Run();
+
+
